8678a512c59399bcd5003f6cb933ca9711b85391
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2024-02-04 06:34:29 +0100

    feat: move-left icon

---
+++ components/icons/move-left.tsx
@@ -0,0 +1,19 @@
+export default function MoveLeftIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m12 19l-7-7l7-7m7 7H5"
+      ></path>
+    </svg>
+  );
+}
